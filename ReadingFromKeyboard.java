320932efc3dc723902d70b680d5e004fadab3c4b
chiragdhunna <chiragdhunna2468@gmail.com>
2024-08-26 20:43:08 +0530

    Read from Keyboard

---
+++ ReadingFromKeyboard.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ReadingFromKeyboard {
+     public static void main(String args[]){
+        Scanner s  = new Scanner(System.in);
+        int x;
+        x = s.nextInt();
+        System.out.println("My First Program " + x);
+        s.close();
+     }
+}
