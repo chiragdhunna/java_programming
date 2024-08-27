bab5d7b8dab476884de3f9e9fd2eff1a33aed0b6
chiragdhunna <chiragdhunna2468@gmail.com>
2024-08-27 18:36:51 +0530

    Data Types in JAVA

---
+++ Data Types/DataTypes.java
@@ -0,0 +1,19 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 100;
+        int i = 100000;
+        long l = 10000000;
+        float f = 1.09f;
+        double d = 10.123;
+        char c = 'a';
+        System.out.println("Size of Byte in Bytes is : " +Byte.BYTES + " and a example variable is "+ b);
+        System.out.println("Size of Short in Bytes is : " +Short.BYTES + " and a example variable is "+ s);
+        System.out.println("Size of Integer in Bytes is : " +Integer.BYTES + " and a example variable is "+ i);
+        System.out.println("Size of Long in Bytes is : " +Long.BYTES + " and a example variable is "+ l);
+        System.out.println("Size of Float in Bytes is : " +Float.BYTES + " and a example variable is "+ f);
+        System.out.println("Size of Double in Bytes is : " +Double.BYTES + " and a example variable is "+ d);
+        System.out.println("Size of Character in Bytes is : " +Character.BYTES + " and a example variable is "+ c);
+    }
+}
