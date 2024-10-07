267c04032f0f8a08305e46fc695fe1c53627d211
chiragdhunna <chiragdhunna2468@gmail.com>
2024-10-07 15:17:36 +0530

    completing the TV Class

--- 10. Inheritance/Method Overriding/TVSmartTV.java
+++ 10. Inheritance/Method Overriding/TVSmartTV.java
@@ -1,11 +1,35 @@
 class TV {
+    TV(){
+        System.out.println("TV Initialised");
+    }
+
+    public void changeChannel(){
+        System.out.println("Channel Channed in TV");
+    }
+    
+    public void setVolume(){
+        System.out.println("Volume set in TV");
+    }
 }
 
 class SmartTV extends TV {
+    SmartTV(){
+        System.out.println("SmartTV Initialised");
+    }
+
+    public void changeChannel(){
+        System.out.println("Channel Channed in SmartTV");
+    }
+    
+    public void setVolume(){
+        System.out.println("Volume set in SmartTV");
+    }
 }
 
 public class TVSmartTV {
     public static void main(String[] args) {
+        TV tv = new SmartTV();
+        tv.changeChannel(); // This is also Called as dynamic method dispatch as method of smartTV is called instead of TV
 
     }
 }
